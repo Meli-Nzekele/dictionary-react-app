003484b6b6afd1b650255981f2fe9fc7a9be9b27
Meli-Nzekele <melissanzekelecourtney@gmail.com>
2022-08-05 11:48:49 +0100

    Dictionary.js/css created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import React from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello Dictionary App</h1>
-      </header>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="Footer mt-3">
+          This App was built by
+          <a
+            href="https://incandescent-tapioca-00054a.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {" "}
+            Melissa Nzekele,
+          </a>{" "}
+          open-sourced on{" "}
+          <a
+            href="https://github.com/Meli-Nzekele/dictionary-react-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          & Hosted on{" "}
+          <a
+            href="https://cheerful-tarsier-e979c2.netlify.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
